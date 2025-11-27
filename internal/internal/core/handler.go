8e91bbedbe2c05642a4f570314dbd1da20413bb1
storezhang <storezhang@gmail.com>
2025-11-27 11:16:35 +0800

    feat(服务器): 增加验证器

--- internal/internal/core/handler.go
+++ internal/internal/core/handler.go
@@ -3,19 +3,24 @@ package core
 import (
 	"net/http"
 
+	"github.com/goexl/log"
 	"github.com/harluo/echo/internal/internal/kernel"
 	"github.com/labstack/echo/v4"
 )
 
-type Handler struct{}
+type Handler struct {
+	logger log.Logger
+}
 
-func newHandler() *Handler {
-	return new(Handler)
+func newHandler(logger log.Logger) *Handler {
+	return &Handler{
+		logger: logger,
+	}
 }
 
 func (h Handler) Handle(handler kernel.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) (err error) {
-		if rsp, he := handler(kernel.NewContext(ctx)); nil != he {
+		if rsp, he := handler(kernel.NewContext(ctx, h.logger)); nil != he {
 			err = he
 		} else {
 			err = ctx.JSON(http.StatusOK, rsp)
