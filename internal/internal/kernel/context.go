90189d00d4348552a7a37bb113165eaa57fffdb8
storezhang <storezhang@gmail.com>
2025-11-27 12:41:31 +0800

    refactor(处理器): 隐藏绑定逻辑

--- internal/internal/kernel/context.go
+++ internal/internal/kernel/context.go
@@ -1,21 +1,44 @@
 package kernel
 
 import (
+	"context"
+	"time"
+
 	"github.com/goexl/log"
 	"github.com/labstack/echo/v4"
 )
 
+var _ context.Context = (*Context)(nil)
+
 type Context struct {
-	context echo.Context
-	logger  log.Logger
+	ec     echo.Context
+	ctx    context.Context
+	logger log.Logger
 }
 
 func NewContext(
-	context echo.Context,
+	ec echo.Context,
 	logger log.Logger,
 ) *Context {
 	return &Context{
-		context: context,
-		logger:  logger,
+		ec:     ec,
+		ctx:    context.Background(),
+		logger: logger,
 	}
+}
+
+func (c Context) Deadline() (time.Time, bool) {
+	return c.ctx.Deadline()
+}
+
+func (c Context) Done() <-chan struct{} {
+	return c.ctx.Done()
+}
+
+func (c Context) Err() error {
+	return c.ctx.Err()
+}
+
+func (c Context) Value(key any) any {
+	return c.ctx.Value(key)
 }
